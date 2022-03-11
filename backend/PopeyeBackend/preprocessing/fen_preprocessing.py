cff2b43c54ee42888ab73e5ceab707661ce87b6e
Jumanji <victormacinic@gmail.com>
2022-03-11 16:09:40 +0200

    added bit-map preprocessing

--- backend/PopeyeBackend/preprocessing/fen_preprocessing.py
+++ backend/PopeyeBackend/preprocessing/fen_preprocessing.py
@@ -3,6 +3,7 @@ import chess.engine
 import chess.pgn
 import os
 from decouple import config
+import numpy as np
 
 
 def fen_to_matrix(fen):
@@ -50,6 +51,92 @@ def fen_to_sparse_matrix6(fen):
             piece_map["B"], piece_map["K"], piece_map["Q"]]
 
 
+def fen_to_sparse_matrix12(fen):
+    piece_map = {
+        "P": [[0 for _ in range(8)] for _ in range(8)],
+        "R": [[0 for _ in range(8)] for _ in range(8)],
+        "N": [[0 for _ in range(8)] for _ in range(8)],
+        "B": [[0 for _ in range(8)] for _ in range(8)],
+        "K": [[0 for _ in range(8)] for _ in range(8)],
+        "Q": [[0 for _ in range(8)] for _ in range(8)],
+        "p": [[0 for _ in range(8)] for _ in range(8)],
+        "r": [[0 for _ in range(8)] for _ in range(8)],
+        "n": [[0 for _ in range(8)] for _ in range(8)],
+        "b": [[0 for _ in range(8)] for _ in range(8)],
+        "k": [[0 for _ in range(8)] for _ in range(8)],
+        "q": [[0 for _ in range(8)] for _ in range(8)],
+    }
+    records = fen.split()
+    board = records[0]
+    position_rows = board.split("/")
+    contor = 0
+    for position_row in position_rows:
+        for potential_piece in position_row:
+            if potential_piece.isalpha():
+                piece_map[potential_piece][int(contor / 8)][contor % 8] = 1
+                contor += 1
+            else:
+                contor += int(potential_piece)
+    return [piece_map["P"], piece_map["R"], piece_map["N"],
+            piece_map["B"], piece_map["K"], piece_map["Q"], piece_map["p"], piece_map["r"], piece_map["n"],
+            piece_map["b"], piece_map["k"], piece_map["q"]]
+
+def getLines(inputFilePath):
+    with open(inputFilePath, 'r') as file:
+        lines=file.readlines()
+    return lines
+
+def getTrainDataDenseMatrix(inputFilePath):
+    X_train = np.empty(shape=(0, 8, 8))
+    Y_train = np.empty(shape=(0))
+    with open(inputFilePath, 'r') as file:
+        for line in file:
+            line_elems = line.split(":")
+            fen, evaluation = line_elems[0], line_elems[1]
+            if "#" in evaluation:
+                continue
+            X_train = np.append(X_train, np.array([fen_to_matrix(fen)]), axis=0)
+            Y_train = np.append(Y_train, np.array([int(evaluation)]), axis=0)
+    return X_train, Y_train
+
+
+def getTrainDataSparseMatrix6(inputFilePath):
+    X_train = np.empty(shape=(0, 6, 8, 8))
+    Y_train = np.empty(shape=(0))
+    with open(inputFilePath, 'r') as file:
+        for line in file:
+            line_elems = line.split(":")
+            fen, evaluation = line_elems[0], line_elems[1]
+            if "#" in evaluation:
+                continue
+            X_train = np.append(X_train, np.array([fen_to_sparse_matrix6(fen)]), axis=0)
+            Y_train = np.append(Y_train, np.array([int(evaluation)]), axis=0)
+    return X_train, Y_train
+
+
+def getTrainDataSparseMatrix12Classification(lines):
+    X_train = np.zeros(shape=(len(lines), 12, 8, 8))
+    Y_train = np.zeros(shape=(len(lines), 1))
+    i = 0
+    for idx, line in enumerate(lines):
+        line = line.strip()
+        line_elems = line.split(":")
+        fen, evaluation = line_elems[0], line_elems[1]
+        if "#" in evaluation:
+            continue
+        X_train[idx] = np.array(fen_to_sparse_matrix12(fen))
+        if int(evaluation) < -100:
+            Y_train[idx] = np.array([0])
+        elif -100 < int(evaluation) < 100:
+            Y_train[idx] = np.array([1])
+        else:
+            Y_train[idx] = np.array([2])
+        i += 1
+        if i % 10000 == 0:
+            print("Gata inca 10000 suntem la : " + str(i))
+    return X_train, Y_train
+
+
 def save_train_data(fen_dict, batchPath):
     with open(batchPath + "/trainData", 'a') as file:
         for fen in fen_dict:
@@ -61,16 +148,14 @@ def save_checkpoint(batchPath, idxPgn, idxGame):
         file.write(str(idxPgn) + ":" + str(idxGame))
 
 
-def readCheckpoint(batchPath):
+def read_checkpoint(batchPath):
     with open(batchPath + "/checkpoint.txt", "r") as file:
         idxGame, idxCheckpont = file.read().strip().split(":")
     return int(idxGame), int(idxCheckpont)
 
 
 def create_labels(batchPath):
-    
-    
-    idxGame, idxPgn = readCheckpoint(batchPath)
+    idxGame, idxPgn = read_checkpoint(batchPath)
 
     engine = chess.engine.SimpleEngine.popen_uci("../../stockfish/stockfish.exe")
     for idx, pgn_file in enumerate(os.listdir(batchPath)):
@@ -86,7 +171,7 @@ def create_labels(batchPath):
                 if i % 10 != 0:
                     print("Saving...")
                     save_train_data(fen_dict, batchPath)
-                    save_checkpoint(batchPath,idx, i)
+                    save_checkpoint(batchPath, idx, i)
                     fen_dict.clear()
                 break
             if idx == idxGame and i <= idxPgn:
@@ -101,17 +186,36 @@ def create_labels(batchPath):
             if i % 10 == 0:
                 print("Saving...")
                 save_train_data(fen_dict, batchPath)
-                save_checkpoint(batchPath,idx, i)
+                save_checkpoint(batchPath, idx, i)
                 fen_dict.clear()
             i += 1
         pgn.close()
     engine.quit()
 
 
+def remove_duplicates(input_file, output_file):
+    fens = set()
+    evaluations = []
+    i = 0
+    with open(input_file, 'r') as file:
+        for line in file:
+            line_elems = line.split(":")
+            fen, evaluation = line_elems[0], line_elems[1]
+            if "#" in evaluation:
+                continue
+            if fen not in fens:
+                fens.add(fen)
+                evaluations.append(evaluation)
+            i += 1
+            if i % 10000 == 0:
+                print("Gata inca 10000 suntem la : " + str(i))
+    with open(output_file, 'w') as file:
+        for fen, eval in zip(fens, evaluations):
+            file.write(fen + ":" + str(eval))
+
+
 def main():
-    
-    BATCH_PATH = config("PATH_TO_CURRENT_BATCH")
-    create_labels(BATCH_PATH)
+    remove_duplicates("../trainData","../theData")
 
 
 if __name__ == '__main__':
