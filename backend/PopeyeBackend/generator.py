860f9470b829507045cd8af0ea404a40140b5efa
Jumanji <victormacinic@gmail.com>
2022-03-12 20:05:18 +0200

    fixed bugs in generator

--- backend/PopeyeBackend/generator.py
+++ backend/PopeyeBackend/generator.py
@@ -8,7 +8,6 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __init__(self, fileName, batchSize, inputSize, outputSize, validationSplit=0.2, train=True,
                  shuffle=True):
         self.__fileName = fileName
-        self.__numOfData = self.__getNumOfData()
         self.__batchSize = batchSize
         self.__shuffle = shuffle
         self.__inputSize = inputSize
@@ -16,12 +15,14 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.__train = train
         self.__validationSplit = validationSplit
         with open(self.__fileName, 'r') as file:
+            numOfAllData = self.__getNumOfData()
             if self.__train:
-                size = int(self.__numOfData * (1 - self.__validationSplit)) - 1
+                size = int(numOfAllData * (1 - self.__validationSplit))
                 self.__lines = file.readlines()[:size]
             else:
-                size = int(self.__numOfData * (1 - self.__validationSplit)) - 1
+                size = int(numOfAllData * (1 - self.__validationSplit))
                 self.__lines = file.readlines()[size:]
+        self.__numOfData = len(self.__lines)
         self.on_epoch_end()
 
     def __getNumOfData(self):
@@ -33,12 +34,12 @@ class DataGenerator(tf.keras.utils.Sequence):
         return int(np.ceil(self.__numOfData / self.__batchSize))
 
     def __getitem__(self, index):
-        if index == self.__len__() - 1:
+        if index == self.__len__() - 1 and self.__numOfData % self.__batchSize != 0:
             X = np.zeros(shape=(self.__numOfData % self.__batchSize, *self.__inputSize))
             Y = np.zeros(shape=(self.__numOfData % self.__batchSize, *self.__outputSize))
-            for i, line in enumerate(self.__lines[index * self.__batchSize:]):
-                fen = line.strip().split(":")[0]
-                eval = line.strip().split(":")[1]
+            for i in range(self.__numOfData % self.__batchSize):
+                fen = self.__lines[index * self.__batchSize + i].strip().split(":")[0]
+                eval = self.__lines[index * self.__batchSize + i].strip().split(":")[1]
                 X[i] = np.transpose(fp.fen_to_sparse_matrix12(fen))
                 if int(eval) < -100:
                     Y[i] = np.array([0])
@@ -47,12 +48,12 @@ class DataGenerator(tf.keras.utils.Sequence):
                 else:
                     Y[i] = np.array([2])
         else:
-            X = np.zeros(shape=(self.__batchSize,*self.__inputSize))
-            Y = np.zeros(shape=(self.__batchSize,*self.__outputSize))
-            for i,line in enumerate(self.__lines[index*self.__batchSize:(index+1)*self.__batchSize]):
-                fen=line.strip().split(":")[0]
-                eval=line.strip().split(":")[1]
-                X[i]=np.transpose(fp.fen_to_sparse_matrix12(fen))
+            X = np.zeros(shape=(self.__batchSize, *self.__inputSize))
+            Y = np.zeros(shape=(self.__batchSize, *self.__outputSize))
+            for i in range(self.__batchSize):
+                fen = self.__lines[index * self.__batchSize + i].strip().split(":")[0]
+                eval = self.__lines[index * self.__batchSize + i].strip().split(":")[1]
+                X[i] = np.transpose(fp.fen_to_sparse_matrix12(fen))
                 if int(eval) < -100:
                     Y[i] = np.array([0])
                 elif -100 < int(eval) < 100:
@@ -61,9 +62,6 @@ class DataGenerator(tf.keras.utils.Sequence):
                     Y[i] = np.array([2])
         return X, Y
 
-    def __shuffleData(self):
-        np.random.shuffle(self.__lines)
-
     def on_epoch_end(self):
         if self.__shuffle:
-            self.__shuffleData()
+            np.random.shuffle(self.__lines)
--- backend/PopeyeBackend/train.py
+++ backend/PopeyeBackend/train.py
@@ -7,7 +7,6 @@ from preprocessing import fen_preprocessing as fp
 
 def get_conv_model_regression():
     model = tf.keras.models.Sequential([
-        # tf.keras.layers.Input(shape=(8, 8, 1)),
         tf.keras.layers.Input(shape=(8, 8, 6)),
         tf.keras.layers.Conv2D(filters=8, kernel_size=7, padding='same', kernel_initializer='lecun_normal',
                                activation='selu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
@@ -43,31 +42,31 @@ def get_conv_model_regression():
 def get_conv_model_classification():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(8, 8, 12)),
-        tf.keras.layers.Conv2D(filters=16, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=16, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=32, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=32, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=64, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=64, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=128, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=128, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Conv2D(filters=256, kernel_size=7, padding='same', kernel_initializer='he_uniform',
-                               activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-
+        tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
+        tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding='same', kernel_initializer='he_uniform',
+                               activation='relu'),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
-        tf.keras.layers.Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)),
+        tf.keras.layers.Dense(4096, activation='relu'),
+        tf.keras.layers.Dense(4096, activation='relu'),
+        tf.keras.layers.Dense(2048, activation='relu'),
+        tf.keras.layers.Dense(2048, activation='relu'),
         tf.keras.layers.Dense(3, activation='softmax'),
     ])
     model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
@@ -112,4 +111,7 @@ if __name__=="__main__":
     valGen = DataGenerator("theData", BATCH_SIZE, (8, 8, 12), (1,), train=False)
     model=get_conv_model_classification()
     EPOCHS = int(config("EPOCHS"))
-    model.fit(trainGen,validation_data=valGen,epochs=EPOCHS)
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir='./logsConvModel'),
+    ]
+    model.fit(trainGen,validation_data=valGen,epochs=EPOCHS, callbacks=callbacks)
